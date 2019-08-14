e020afd0e436bf943d848a987fdd6b3f4477d42e
observateur1980 <44187905+observateur1980@users.noreply.github.com>
2019-08-13 23:39:34 -0700

    sasa

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from . models import Article
 
 # Register your models here.
+
+class ArticleModelAdmin(admin.ModelAdmin):
+    fields = [
+        'id',
+        'publish',
+        'title',
+        'text',
+        'articlecover',
+       
+        
+
+
+    ]
+
+    readonly_fields = ['id',]
+
+    list_display = ('title',
+                    'publish',)
+
+    class Meta:
+        model = Article
+
+
+
+
+admin.site.register(Article, ArticleModelAdmin)
--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-
+from . models import Article
 # Create your views here.
 
 class Home(TemplateView):
 	template_name = 'home/home.html'
+	article = Article.objects.first_articles()
+	
+	def get_context_data(self, *args, **kwargs):
+		context = super(Home, self).get_context_data(*args, **kwargs)		
+		context["bck_image"] = 'images/index_hero.jpg'
+		context["active_page"] = 'home'
+		context["article"] = self.article
+		return context
