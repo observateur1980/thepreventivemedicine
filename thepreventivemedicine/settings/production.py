a9d359e209397fb9fb90fad16d07293df3bd9ee3
observateur1980 <44187905+observateur1980@users.noreply.github.com>
2019-09-04 00:41:41 -0700

    aa

--- thepreventivemedicine/settings/production.py
+++ thepreventivemedicine/settings/production.py
@@ -1,7 +1,7 @@
 
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['www.thepreventivemedicine.com', 'thepreventivemedicine.com']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
